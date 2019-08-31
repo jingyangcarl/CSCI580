8c75e87e281060ed51d113227b0296e8cc20ecdd
Jing Yang <jingyang_carl@qq.com>
2019-08-31 11:53:10 -0700

    Update rend.cpp

--- Homwork1/rend.cpp
+++ Homwork1/rend.cpp
@@ -15,18 +15,31 @@ GzRender::GzRender(int xRes, int yRes)
  -- allocate memory for framebuffer : 3 bytes(b, g, r) x width x height
  -- allocate memory for pixel buffer
  */
+	this->xres = xRes;
+	this->yres = yRes;
+	this->framebuffer = new char[3 * sizeof(byte) * xRes * yRes];
+	this->pixelbuffer = new GzPixel[xRes * yRes];
 }
 
 GzRender::~GzRender()
 {
 /* HW1.2 clean up, free buffer memory */
-
+	delete this->framebuffer;
+	delete this->pixelbuffer;
 }
 
 int GzRender::GzDefault()
 {
 /* HW1.3 set pixel buffer to some default values - start a new frame */
-
+	for (int i = 0; i < xres; i++) {
+		for (int j = 0; j < yres; j++) {
+			pixelbuffer[i * xres + j].red = 255;
+			pixelbuffer[i * xres + j].green = 255;
+			pixelbuffer[i * xres + j].blue = 255;
+			pixelbuffer[i * xres + j].alpha = 255;
+			pixelbuffer[i * xres + j].z = 0;
+		}
+	}
 	return GZ_SUCCESS;
 }
 
@@ -34,7 +47,11 @@ int GzRender::GzDefault()
 int GzRender::GzPut(int i, int j, GzIntensity r, GzIntensity g, GzIntensity b, GzIntensity a, GzDepth z)
 {
 /* HW1.4 write pixel values into the buffer */
-
+	pixelbuffer[i * xres + j].red = r;
+	pixelbuffer[i * xres + j].green = g;
+	pixelbuffer[i * xres + j].blue = b;
+	pixelbuffer[i * xres + j].alpha = a;
+	pixelbuffer[i * xres + j].z = z;
 	return GZ_SUCCESS;
 }
 
@@ -42,7 +59,11 @@ int GzRender::GzPut(int i, int j, GzIntensity r, GzIntensity g, GzIntensity b, G
 int GzRender::GzGet(int i, int j, GzIntensity *r, GzIntensity *g, GzIntensity *b, GzIntensity *a, GzDepth *z)
 {
 /* HW1.5 retrieve a pixel information from the pixel buffer */
-
+	*r = pixelbuffer[i * xres + j].red;
+	*g = pixelbuffer[i * xres + j].green;
+	*b = pixelbuffer[i * xres + j].blue;
+	*a = pixelbuffer[i * xres + j].alpha;
+	*z = pixelbuffer[i * xres + j].z;
 	return GZ_SUCCESS;
 }
 
@@ -50,7 +71,6 @@ int GzRender::GzGet(int i, int j, GzIntensity *r, GzIntensity *g, GzIntensity *b
 int GzRender::GzFlushDisplay2File(FILE* outfile)
 {
 /* HW1.6 write image to ppm file -- "P6 %d %d 255\r" */
-	
 	return GZ_SUCCESS;
 }
 
