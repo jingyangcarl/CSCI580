d5bf72a70ac5b364b2497d04f20d73ccfcc4f339
Jing Yang <jingyang_carl@qq.com>
2019-08-31 17:26:30 -0700

    finish GzFlushDisplay2File and GzFlushDisplay2FrameBuffer

--- Homwork1/rend.cpp
+++ Homwork1/rend.cpp
@@ -7,6 +7,11 @@
 #include    "stdafx.h"  
 #include	"Gz.h"
 
+#include <string>
+#include <iostream>
+#include <sstream>
+
+
 
 GzRender::GzRender(int xRes, int yRes)
 {
@@ -17,7 +22,7 @@ GzRender::GzRender(int xRes, int yRes)
  */
 	this->xres = xRes;
 	this->yres = yRes;
-	this->framebuffer = new char[3 * sizeof(byte) * xRes * yRes];
+	this->framebuffer = new char[3 * sizeof(char) * xRes * yRes];
 	this->pixelbuffer = new GzPixel[xRes * yRes];
 }
 
@@ -33,11 +38,11 @@ int GzRender::GzDefault()
 /* HW1.3 set pixel buffer to some default values - start a new frame */
 	for (int i = 0; i < xres; i++) {
 		for (int j = 0; j < yres; j++) {
-			pixelbuffer[i * xres + j].red = 255;
-			pixelbuffer[i * xres + j].green = 255;
-			pixelbuffer[i * xres + j].blue = 255;
-			pixelbuffer[i * xres + j].alpha = 255;
-			pixelbuffer[i * xres + j].z = 0;
+			pixelbuffer[j * xres + i].red = 4095;
+			pixelbuffer[j * xres + i].green = 4095;
+			pixelbuffer[j * xres + i].blue = 4095;
+			pixelbuffer[j * xres + i].alpha = 1;
+			pixelbuffer[j * xres + i].z = 0;
 		}
 	}
 	return GZ_SUCCESS;
@@ -47,11 +52,15 @@ int GzRender::GzDefault()
 int GzRender::GzPut(int i, int j, GzIntensity r, GzIntensity g, GzIntensity b, GzIntensity a, GzDepth z)
 {
 /* HW1.4 write pixel values into the buffer */
-	pixelbuffer[i * xres + j].red = r;
-	pixelbuffer[i * xres + j].green = g;
-	pixelbuffer[i * xres + j].blue = b;
-	pixelbuffer[i * xres + j].alpha = a;
-	pixelbuffer[i * xres + j].z = z;
+	if (i >= 0 && i < xres && j >= 0 && j < yres) {
+		pixelbuffer[j * xres + i].red = r < 4095 ? r : 4095;
+		pixelbuffer[j * xres + i].green = g < 4095 ? g : 4095;
+		pixelbuffer[j * xres + i].blue = b < 4095 ? b : 4095;
+		pixelbuffer[j * xres + i].alpha = a;
+		pixelbuffer[j * xres + i].z = z;
+	} else {
+
+	}
 	return GZ_SUCCESS;
 }
 
@@ -59,11 +68,15 @@ int GzRender::GzPut(int i, int j, GzIntensity r, GzIntensity g, GzIntensity b, G
 int GzRender::GzGet(int i, int j, GzIntensity *r, GzIntensity *g, GzIntensity *b, GzIntensity *a, GzDepth *z)
 {
 /* HW1.5 retrieve a pixel information from the pixel buffer */
-	*r = pixelbuffer[i * xres + j].red;
-	*g = pixelbuffer[i * xres + j].green;
-	*b = pixelbuffer[i * xres + j].blue;
-	*a = pixelbuffer[i * xres + j].alpha;
-	*z = pixelbuffer[i * xres + j].z;
+	if (i >= 0 && i < xres && j >= 0 && j < yres) {
+		*r = pixelbuffer[j * xres + i].red;
+		*g = pixelbuffer[j * xres + i].green;
+		*b = pixelbuffer[j * xres + i].blue;
+		*a = pixelbuffer[j * xres + i].alpha;
+		*z = pixelbuffer[j * xres + i].z;
+	} else {
+
+	}
 	return GZ_SUCCESS;
 }
 
@@ -71,6 +84,16 @@ int GzRender::GzGet(int i, int j, GzIntensity *r, GzIntensity *g, GzIntensity *b
 int GzRender::GzFlushDisplay2File(FILE* outfile)
 {
 /* HW1.6 write image to ppm file -- "P6 %d %d 255\r" */
+	std::stringstream ss;
+	ss << "P6 " << std::to_string(xres) << " " << std::to_string(yres) << " 255\r";
+	for (int i = 0; i < xres; i++) {
+		for (int j = 0; j < yres; j++) {
+			ss << char(pixelbuffer[i * xres + j].red >> 4);
+			ss << char(pixelbuffer[i * xres + j].green >> 4);
+			ss << char(pixelbuffer[i * xres + j].blue >> 4);
+		}
+	}
+	fwrite(ss.str().data(), ss.str().size(), 1, outfile);
 	return GZ_SUCCESS;
 }
 
@@ -81,6 +104,12 @@ int GzRender::GzFlushDisplay2FrameBuffer()
 	- CAUTION: when storing the pixels into the frame buffer, the order is blue, green, and red 
 	- NOT red, green, and blue !!!
 */
-
+	for (int i = 0; i < xres; i++) {
+		for (int j = 0; j < yres; j++) {
+			framebuffer[3 * (j * xres + i) + 0] = char(pixelbuffer[j * xres + i].blue >> 4);
+			framebuffer[3 * (j * xres + i) + 1] = char(pixelbuffer[j * xres + i].green >> 4);
+			framebuffer[3 * (j * xres + i) + 2] = char(pixelbuffer[j * xres + i].red >> 4);
+		}
+	}
 	return GZ_SUCCESS;
 }
