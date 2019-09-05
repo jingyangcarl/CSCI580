6fb9cbc6df0ca55c738dfd924aaa4e76482ddd5b
Jing Yang <jingyang_carl@qq.com>
2019-09-04 21:52:21 -0700

    Homework2 Initialization

---
+++ Homework2/rend.cpp
@@ -0,0 +1,94 @@
+#include	"stdafx.h"
+#include	"stdio.h"
+#include	"math.h"
+#include	"Gz.h"
+#include	"rend.h"
+
+/***********************************************/
+/* HW1 methods: copy here the methods from HW1 */
+
+GzRender::GzRender(int xRes, int yRes)
+{
+/* HW1.1 create a framebuffer for MS Windows display:
+ -- set display resolution
+ -- allocate memory for framebuffer : 3 bytes(b, g, r) x width x height
+ -- allocate memory for pixel buffer
+ */
+}
+
+GzRender::~GzRender()
+{
+/* HW1.2 clean up, free buffer memory */
+
+}
+
+int GzRender::GzDefault()
+{
+/* HW1.3 set pixel buffer to some default values - start a new frame */
+
+	return GZ_SUCCESS;
+}
+
+
+int GzRender::GzPut(int i, int j, GzIntensity r, GzIntensity g, GzIntensity b, GzIntensity a, GzDepth z)
+{
+/* HW1.4 write pixel values into the buffer */
+
+	return GZ_SUCCESS;
+}
+
+
+int GzRender::GzGet(int i, int j, GzIntensity *r, GzIntensity *g, GzIntensity *b, GzIntensity *a, GzDepth *z)
+{
+/* HW1.5 retrieve a pixel information from the pixel buffer */
+
+	return GZ_SUCCESS;
+}
+
+
+int GzRender::GzFlushDisplay2File(FILE* outfile)
+{
+/* HW1.6 write image to ppm file -- "P6 %d %d 255\r" */
+	
+	return GZ_SUCCESS;
+}
+
+int GzRender::GzFlushDisplay2FrameBuffer()
+{
+/* HW1.7 write pixels to framebuffer: 
+	- put the pixels into the frame buffer
+	- CAUTION: when storing the pixels into the frame buffer, the order is blue, green, and red 
+	- NOT red, green, and blue !!!
+*/
+
+	return GZ_SUCCESS;
+}
+
+
+/***********************************************/
+/* HW2 methods: implement from here */
+
+int GzRender::GzPutAttribute(int numAttributes, GzToken	*nameList, GzPointer *valueList) 
+{
+/* HW 2.1
+-- Set renderer attribute states (e.g.: GZ_RGB_COLOR default color)
+-- In later homeworks set shaders, interpolaters, texture maps, and lights
+*/
+
+	return GZ_SUCCESS;
+}
+
+int GzRender::GzPutTriangle(int	numParts, GzToken *nameList, GzPointer *valueList) 
+/* numParts - how many names and values */
+{
+/* HW 2.2
+-- Pass in a triangle description with tokens and values corresponding to
+      GZ_NULL_TOKEN:		do nothing - no values
+      GZ_POSITION:		3 vert positions in model space
+-- Invoke the rastrizer/scanline framework
+-- Return error code
+*/
+
+	return GZ_SUCCESS;
+}
+
---
+++ Homework2/rend.h
@@ -0,0 +1,82 @@
+
+#include	"gz.h"
+#ifndef GZRENDER_
+#define GZRENDER_
+
+
+/* Camera defaults */
+#define	DEFAULT_FOV		35.0
+#define	DEFAULT_IM_Z	(-10.0)  /* world coords for image plane origin */
+#define	DEFAULT_IM_Y	(5.0)    /* default look-at point = 0,0,0 */
+#define	DEFAULT_IM_X	(-10.0)
+
+#define	DEFAULT_AMBIENT	{0.1, 0.1, 0.1}
+#define	DEFAULT_DIFFUSE	{0.7, 0.6, 0.5}
+#define	DEFAULT_SPECULAR	{0.2, 0.3, 0.4}
+#define	DEFAULT_SPEC		32
+
+#define	MATLEVELS	10		/* how many matrix pushes allowed */
+#define	MAX_LIGHTS	10		/* how many lights allowed */
+
+/* Dummy definition : change it later */
+#ifndef GzCamera
+#define GzCamera	    	GzPointer
+#endif
+
+#ifndef GzLight
+#define GzLight		GzPointer
+#endif
+
+#ifndef GzTexture
+#define GzTexture	GzPointer
+#endif
+
+
+
+class GzRender{			/* define a renderer */
+  
+
+public:
+	unsigned short	xres;
+	unsigned short	yres;
+	GzPixel		*pixelbuffer;		/* frame buffer array */
+	char* framebuffer;
+
+	GzCamera		camera;
+	short		matlevel;	/* top of stack - current xform */
+	GzMatrix		Ximage[MATLEVELS];	/* stack of xforms (Xsm) */
+	GzMatrix		Xnorm[MATLEVELS];	/* xform for norms (Xim) */
+	GzMatrix		Xsp;		/* NDC to screen (pers-to-screen) */
+	GzColor		flatcolor;    /* color state for flat shaded triangles */
+	int			interp_mode;
+	int			numlights;
+	GzLight		lights[MAX_LIGHTS];
+	GzLight		ambientlight;
+	GzColor		Ka, Kd, Ks;
+	float		spec;		/* specular power */
+	GzTexture		tex_fun;  /* tex_fun(float u, float v, GzColor color) */
+
+
+	// Constructors
+	GzRender(int xRes, int yRes);
+	~GzRender();
+
+	// HW1: Display methods
+	int GzDefault();
+	int GzPut(int i, int j, GzIntensity r, GzIntensity g, GzIntensity b, GzIntensity a, GzDepth z);
+	int GzGet(int i, int j, GzIntensity *r, GzIntensity *g, GzIntensity *b, GzIntensity *a, GzDepth	*z);
+
+	int GzFlushDisplay2File(FILE* outfile);
+	int GzFlushDisplay2FrameBuffer();
+
+	// HW2: Render methods
+	int GzPutAttribute(int numAttributes, GzToken *nameList, GzPointer *valueList);
+	int GzPutTriangle(int numParts, GzToken *nameList, GzPointer *valueList);
+	
+	// Extra methods: NOT part of API - just for general assistance */
+	inline int ARRAY(int x, int y){return x+(y*xres);}	/* simplify fbuf indexing */
+	inline short	ctoi(float color) {return(short)((int)(color * ((1 << 12) - 1)));}		/* convert float color to GzIntensity short */
+};
+
+#endif
+
